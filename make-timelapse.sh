daa305974c0b7eac71859b05240d1384cb08cb15
nomadhermit <franzdep@gmail.com>
2026-05-21 17:14:53 -0400

    Add script to generate timelapse video from images
    Initial script to create a timelapse video from JPG images with consistent dimensions and embedded timestamps.

---
+++ make-timelapse.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#  initial hack collection of commands to get an end result... optimization and cleanup pending
+
+# make dimansions consistent
+mkdir output
+for i in *.jpg; do      time=$(echo $i | cut -d . -f 1);     ffmpeg -nostdin -y -i $i -vf "scale=1920:1080:force_original_aspect_ratio=>
+
+cd output
+# embed filename in image (removing '_001' from name)
+mkdir raw
+for i in *.jpg; do      time=$(echo $i | cut -d . -f 1 | rev | cut -c 5- | rev )'(EST)';     ffmpeg -nostdin -y -i $i -vf "drawtext=tex>
+
+cd raw
+# create souce file list for ffmpeg
+ls  mod*.jpg | cat -n | while read n f; do echo "file '$f'"; done > list.txt
+
+# generate video from files
+ffmpeg -nostdin -f concat -safe 0 -i list.txt -vf "setpts=3*PTS" -c:v libx264 -pix_fmt yuv420p timelapse.mp4
+
